739f1772b6e109d6e37fcabac7b3008a4e204501
crwn1337 <38888474+crwn1337@users.noreply.github.com>
2021-10-14 13:24:49 +0300

    copypaste fail

--- taskmgr/hooks.cpp
+++ taskmgr/hooks.cpp
@@ -11,7 +11,7 @@ namespace hooks {
 	bool __fastcall IsServer_hook(void* a) {
 		if (!config.globalsettings) {
 			config.globalsettings = a;
-			printf("g_GlobalSettings: %p\n", a);
+			printf("g_GlobalSettings: 0x%p\n", a);
 			config.original_cores = *(uint16_t*)((size_t)a + 0x944);
 		}
 		*(uint16_t*)((size_t)a + 0x944) = config.cores;
@@ -24,25 +24,21 @@ namespace hooks {
 		return config.SetBlockData_original(a1, core, string.data(), color, color);
 	}
 
-	__int64 __fastcall SetRefreshRate_hook(void* a1, int a2) {
-		return config.SetRefreshRate_original(a1, a2);
-	}
-
 	void init() {
 		MH_Initialize();
 		auto IsServer = utils::find_pattern("Taskmgr.exe", "40 53 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 83 B9");
-		printf("IsServer: 0x%llx\n", IsServer);
+		printf("IsServer: 0x%p\n", (void*)IsServer);
 		MH_CreateHook((void*)IsServer, IsServer_hook, (void**)&config.IsServer_original);
 		MH_QueueEnableHook((void*)IsServer);
 
 		auto SetBlockData = utils::find_pattern("Taskmgr.exe", "48 89 5C 24 ? 48 89 6C 24 ? 48 89 74 24 ? 57 41 56 41 57 48 83 EC ? 48 8B 41 ? 48 8B F9");
-		printf("SetBlockData: 0x%llx\n", SetBlockData);
+		printf("SetBlockData: 0x%p\n", (void*)SetBlockData);
 		MH_CreateHook((void*)SetBlockData, SetBlockData_hook, (void**)&config.SetBlockData_original);
 		MH_QueueEnableHook((void*)SetBlockData);
 
 		auto SetRefreshRate = utils::find_pattern("Taskmgr.exe", "48 89 5C 24 ? 48 89 74 24 ? 57 48 83 EC ? 48 8B 3D ? ? ? ? 8B F2");
 		config.SetRefreshRate_original = decltype(config.SetRefreshRate_original)(SetRefreshRate);
-		printf("SetRefreshRate: 0x%llx\n", SetBlockData);
+		printf("SetRefreshRate: 0x%p\n", (void*)SetRefreshRate);
 		config.SetRefreshRate_original(nullptr, 1000 / 60);
 
 		MH_ApplyQueued();
